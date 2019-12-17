xtforgame/az-fullstack-sequelize

Add sequelize code for postgres

src/server/configs/production.js
@@ -32,6 +32,12 @@ const mailerConfig = {
   tokenFile: path.join(secretsFolder, 'gmail', 'token.json'),
 };
 
+const postgresPort = 5432;
+const postgresUser = 'rick';
+const postgresDbName = 'db_rick_data';
+const postgresPassword = 'xxxx1234';
+const postgresHost = 'localhost';
+
 export {
   credentialFiles,
   jwtSecretFiles,
@@ -42,4 +48,10 @@ export {
   externalUrl,
 
   mailerConfig,
+
+  postgresPort,
+  postgresUser,
+  postgresDbName,
+  postgresPassword,
+  postgresHost,
 };

xtforgame/az-minio-sequelize

src/server/configs/production.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import appRootPath from 'app-root-path';
 
+export { jwtIssuer } from 'common/config';
+
 const appRoot = appRootPath.resolve('./');
 const secretsFolder = path.join(appRoot, 'secrets');
 
@@ -32,6 +34,12 @@ const mailerConfig = {
   tokenFile: path.join(secretsFolder, 'gmail', 'token.json'),
 };
 
+const postgresPort = 5432;
+const postgresUser = 'rick';
+const postgresDbName = 'db_rick_data';
+const postgresPassword = 'xxxx1234';
+const postgresHost = 'postgres';
+
 export {
   credentialFiles,
   jwtSecretFiles,
@@ -42,4 +50,10 @@ export {
   externalUrl,
 
   mailerConfig,
+
+  postgresPort,
+  postgresUser,
+  postgresDbName,
+  postgresPassword,
+  postgresHost,
 };